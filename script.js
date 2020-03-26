b71070f23e3b674515b4d6eaf6a3ff4a17b9334e
Plasnid <stuart@sackler.ca>
2020-03-26 13:07:16 -0400

    font-size controls added

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function fontClick(e){
     let myTitle = document.querySelector("h1");
     myTitle.style.fontFamily = e.target.dataset.font;
 }
+function sizeClick(e){
+    console.log(e.target.dataset.size);
+    let myTitle = document.querySelector("h1");
+    myTitle.style.fontSize = e.target.dataset.size;
+}
 
 function btnClick(e){
     console.log("the button was clicked");
@@ -34,25 +39,35 @@ function leaveAction(e){
 
 function setupButtonClicks(){
     console.log("button clicks setup");
-    let btn1 = document.querySelector("#controls>div:nth-child(1)");
-    let btn2 = document.querySelector("#controls>div:nth-child(2)");
-    let btn3 = document.querySelector("#controls>div:nth-child(3)");
-    let btn4 = document.querySelector("#controls>div:nth-child(4)");
-    let btn5 = document.querySelector("#controls>div:nth-child(5)");
-    let btn6 = document.querySelector("#controls>div:nth-child(6)");
+    let btn1 = document.querySelector("#colour_controls>:nth-child(2)");
+    let btn2 = document.querySelector("#colour_controls>:nth-child(3)");
+    let btn3 = document.querySelector("#colour_controls>:nth-child(4)");
+    let btn4 = document.querySelector("#colour_controls>:nth-child(5)");
+    let btn5 = document.querySelector("#colour_controls>:nth-child(6)");
+    let btn6 = document.querySelector("#colour_controls>:nth-child(7)");
 
-    let font1 = document.querySelector("#controls>div:nth-child(7)");
-    let font2 = document.querySelector("#controls>div:nth-child(8)");
+    let font1 = document.querySelector("#font_controls>div:nth-child(2)");
+    let font2 = document.querySelector("#font_controls>div:nth-child(3)");
+
+    let size1 = document.querySelector("#size_controls>:nth-child(2)");
+    let size2 = document.querySelector("#size_controls>:nth-child(3)");
+    console.log(size1);
+    console.log(size2);
     let buttons = [btn1, btn2, btn3,btn4,btn5,btn6];
     let fonties = [font1, font2];
+    let sizeBtns = [size1, size2];
     for(let i=0;i<fonties.length;i++){
         fonties[i].addEventListener("click", fontClick);
+    }
+    for(let i=0;i<sizeBtns.length;i++){
+        sizeBtns[i].addEventListener("click", sizeClick);
     }
     for(let i=0;i<buttons.length;i++){
         buttons[i].addEventListener("click", btnClick);
         buttons[i].addEventListener("mouseenter",enterAction);
         buttons[i].addEventListener("mouseleave", leaveAction);
     }
+    
 }
 
 //function that runs when the page has loaded
